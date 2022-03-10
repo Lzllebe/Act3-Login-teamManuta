be404089da982ebf13042b1ba0cc55a089060116
Manuta30KError <tkelvmanuta@gmail.com>
2022-03-11 00:26:46 +0800

    done activity 3

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,179 +1,205 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import {  StyleSheet, Text,TouchableOpacity } from 'react-native';
 import { RootTabScreenProps } from '../types';
+import React, { useState } from 'react';
+import ViewWithLoading from '../components/ViewWithLoading';
+import { View } from '../components/Themed';
+import { Button } from 'react-native-elements';
+import { TextInput } from 'react-native-paper';
+import HeaderLottie from './HeaderLottie';
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+
+  const [loading,setLoading]=useState<boolean>(false);
+  
+
+  setTimeout(() => {
+   setLoading(false)
+  }, 5000 );
+
   return (
-  // Grand Parent
-  <View style={{
-    flex: 1,
-     flexDirection: 'column',
-   justifyContent: 'flex-start',
-     backgroundColor: '#EEEDDE',
-     paddingTop: 30
-  }}>
-        <Text style={{
+    <ViewWithLoading
+              loading= {false}
+              
+      >
+     
+      <View style= {
+        [
+          styles.container
+        ] }>
+      
+        <Button
+                containerStyle={{
+                  width: 200,
+                  marginLeft: 250,
+                }}
+                title="Sign Up"
+                type="clear"
+                titleStyle={{ color: 'rgba(149, 165, 166)' }}
+        />
+
+        
+            <Text style={{
+              fontWeight: "bold",
+              fontSize: 25,
+                  color: 'black',
+                  paddingTop: 25,
+                  marginLeft: 50,
+                  marginBottom: 49
+            }}>Log In</Text>
+
+      <View style={
+        [
+          styles.textboxcontainer
+        ]
+      }>
+           <TextInput
+            label="Username"
+          />
+      </View>
+      <View style={
+        [
+          styles.textboxcontainer
+        ]  
+      }>
+          <TextInput
+          label="Password"
+          secureTextEntry
+          right={<TextInput.Icon name="eye" />}
+        />
+      </View>
+     <View style = {{
+       backgroundColor : '#EEEDDE',
+       flexDirection: 'row',
+       justifyContent: 'center',
+       marginTop: 10
+     }}><Button
+                title="Log in"
+                loading={false}
+                loadingProps={{ size: 'small', color: 'white' }}
+                buttonStyle={{
+                  backgroundColor: 'black',
+                  borderRadius: 90,
+                }}
+                titleStyle={{ fontWeight: 'bold', fontSize: 23 }}
+                containerStyle={{
+               //   marginHorizontal: 50,
+                  height: 50,
+                  width: 300,
+                //  marginVertical: 10,
+                }}
+                onPress={() => console.log('aye')}
+         />
+         </View>
+
+        
+
+          <Text style={{
+          marginTop: 25,
           fontSize: 15,
-              color: 'gray',
-              marginLeft: 270,
-          }}>Sign Up</Text>
+          color: 'black',
+          textAlign: 'center'
+        }}>Or sign in with social account</Text>
 
-        <Text style={{
-          fontWeight: "bold",
-          fontSize: 25,
-              color: 'black',
-              paddingTop: 25,
-              marginLeft: 50,
-              marginBottom: 10
-        }}>Log In</Text>
-
-      <View style= {{
-        backgroundColor: '#EEEDDE',
-          height: '7%',
-          width: '90%',
-          borderRadius: 15,  
-          marginLeft: 20
-      }}></View>
-
-        <Text style={{
-        color: 'gray',
-        paddingTop: 10,
-        marginLeft: 50,
-        fontWeight:'900'
-      }}>USERNAME</Text>
-
-        <View style={{
-          backgroundColor: '#EEEDDE',
-          borderBottomWidth: 1,
-          height: '7%',
-          width: '90%',
-          borderRadius: 15, 
-          marginBottom: 10,
-          marginLeft: 20
-      }}></View>
-
-        <Text style={{
-          color: 'gray',
-          paddingTop: 10,
-          marginLeft: 50,
-        fontWeight:'900'
-        }}>PASSWORD</Text>
-
-        <View style= {{
-        backgroundColor: '#EEEDDE',
-        borderBottomWidth: 1,
-          height: '7%',
-          width: '90%',
-          borderRadius: 15,  
-          marginLeft: 20
-      }}></View>
-
-        <View style= {{
-          backgroundColor: 'black',
-          height: '8%',
-          width: '90%',
-          borderRadius: 100,
-          marginTop: 20,
-          marginLeft: 18,
-          alignItems:'center',
-          justifyContent: 'center',
-      }}><Text style={{
-        fontSize: 18,
-        fontWeight:'900',
-        color:'white'
-        }}>Login</Text>
-        </View>
-
-        <Text style={{
+         <View style = {{
+            backgroundColor : '#EEEDDE',
+            flexDirection: 'row',
+            justifyContent: 'center',
+            marginTop: 30,
+            flex: 2
+         }}>
+           <Button
+                title="facebook"
+                icon={{
+                  name: 'user',
+                  type: 'font-awesome',
+                  size: 15,
+                  color: 'white',
+                }}
+                iconRight
+                iconContainerStyle={{ marginLeft: 10 }}
+                titleStyle={{ fontWeight: '700' }}
+                buttonStyle={{
+                  backgroundColor: '#3b5998',
+                  borderColor: 'black',
+                  borderWidth: 1,
+                  borderRadius: 30,
+                }}
+                containerStyle={{
+                  width: 150,
+                  height: 200,
+                  
+             //     marginHorizontal: 50,
+             //     marginVertical: 10,
+                    marginRight: 5
+                }}
+              />
+              <Button
+                title="Google"
+                icon={{
+                  name: 'user',
+                  type: 'font-awesome',
+                  size: 15,
+                  color: 'white',
+                }}
+                iconRight
+                iconContainerStyle={{ marginLeft: 10 }}
+                titleStyle={{ fontWeight: '700' }}
+                buttonStyle={{
+                  backgroundColor: '#FF5252',
+                  borderColor: 'black',
+                  borderWidth: 1,
+                  borderRadius: 30,
+                }}
+                containerStyle={{
+                  width: 150,
+                  height: 200,
+                  
+             //     marginHorizontal: 50,
+              //    marginVertical: 10,
+                    marginRight: 5
+                }}
+              />
+           </View>
+                
+           <View style ={{
+        }}> 
+         <TouchableOpacity
+        style={styles.button}
+       >
+        <Text style ={{
           marginTop: 15,
           fontSize: 15,
           textDecorationLine: 'underline',
           color: 'black',
           textAlign: 'center'
-        }}>Forgot Password</Text>
+        }} >Forgot Password</Text>
+         </TouchableOpacity>
+          </View>
+    
+        
 
-        <Text style={{
-          marginTop: 25,
-          fontSize: 15,
-          color: 'black',
-          textAlign: 'center'
-        }}>Or sign up with social account</Text>
-
-       <View style= {{
-              //Code for FB Button and Google Button Design (Batang ina with kambal kids)
-              flexDirection: 'row',
-              justifyContent: 'space-evenly',
-              backgroundColor: '#EEEDDE',
-              width: '100%',
-              height: '100%',
-              marginTop: 25,
-              paddingRight: 20,
-              paddingLeft:2
-            }}>
-                    <View style= {{
-                      // Facebook Button Circle (child 1 ni batang ina)
-                        backgroundColor: '#EEEDDE',
-                        borderTopWidth: 1,
-                        borderBottomWidth: 1,
-                        borderLeftWidth: 1,
-                        borderRightWidth: 1,
-                        height: '9%',
-                        width: '43%',
-                        borderRadius: 100,
-                        alignItems:'center',
-                        justifyContent: 'center',
-                    }}> 
-                            <Text style={{
-                              //Text FB code (apo)
-                                color: 'black',
-                                fontSize: 15, 
-                                fontWeight:'900'
-                            }}>Facebook</Text>
-                    </View>
-      
-                    <View style= {{
-                      // Google Button Circle (child 2 ni batang ina)
-                        backgroundColor: '#EEEDDE',
-                        borderTopWidth: 1,
-                        borderBottomWidth: 1,
-                        borderLeftWidth: 1,
-                        borderRightWidth: 1,
-                        height: '9%',
-                        width: '43%',
-                        borderRadius: 100,
-                        alignItems:'center',
-                        justifyContent: 'center',
-                    }}>     
-                            <Text style={{
-                              //Text Google code (apo)
-                                color: 'black',
-                                fontSize: 15,
-                                fontWeight:'900'
-                            }}>Google</Text>
-                    </View>
-        </View>
-
-    </View>
+  </View>
+  </ViewWithLoading>
  );
 }
 
- 
-     
 const styles = StyleSheet.create({
-  container: {
+  container:{
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+     flexDirection: 'column',
+    justifyContent: 'flex-start',
+     backgroundColor: '#EEEDDE',
+     paddingTop: 10
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  textboxcontainer:{
+    marginHorizontal: 25,
+    marginVertical: 10
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  button: {
+    alignItems: "center",
+    backgroundColor: "#EEEDDE",
+    padding: 10
   },
-});
+}
+)
+    
